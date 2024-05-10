303631270b5350dc99b2269cb9e47059821a246f
Marco Barbone <mbarbone@ccmlin019.flatironinstitute.org>
2024-05-10 12:15:21 -0400

    vectorized both loops, added intrinsics wrapper and tests

--- c++/triqs_ctint/measures/intrinsics.h
+++ c++/triqs_ctint/measures/intrinsics.h
@@ -1,62 +1,98 @@
 #pragma once
+
+#include <iostream>
+
 #ifdef  USE_INTRINSICS
-#include <immintrin.h> // for AVX2 intrinsics
+#include <immintrin.h>
 #include <utility>
 #include <complex>
 
-__attribute__((always_inline, const)) inline constexpr static auto separate_real_imaginary(const __m512d v1, const __m512d v2) noexcept {
-  // Create index vectors for separating real and imaginary parts
-  static constexpr const __m512i idx_real{0, 2, 4, 6, 8, 10, 12, 14};
-  static constexpr const __m512i idx_imag{1, 3, 5, 7, 9, 11, 13, 15};
-  // reverse order of idx_real
-  // Separate real and imaginary parts
-  const auto&& real_v = _mm512_permutex2var_pd(v1, idx_real, v2);
-  const auto&& imag_v = _mm512_permutex2var_pd(v1, idx_imag, v2);
+template <class T, std::uint8_t elems> using Vec = T __attribute__((vector_size(elems * sizeof(T))));
+
+template <std::size_t... I>
+__attribute__((always_inline)) inline static constexpr auto permute_vectors(std::index_sequence<I...>, const auto v1, const auto v2) noexcept {
+  return __builtin_shufflevector(v1, v2, I...);
+}
+
+__attribute__((always_inline)) inline static constexpr auto separate_real_imaginary(const auto &v1, const auto &v2) noexcept {
+  static constexpr auto generate_indices = []<std::size_t N, bool odd>() constexpr {
+    return []<std::size_t... I>(std::index_sequence<I...>) {
+      return std::index_sequence<(2 * I + static_cast<int>(odd))...>{};
+    }(std::make_index_sequence<N>{});
+  };
+  static constexpr auto N            = sizeof(decltype(v1)) / sizeof(decltype(v1[0]));
+  static constexpr auto even_indices = generate_indices.template operator()<N, false>();
+  static constexpr auto odd_indices  = generate_indices.template operator()<N, true>();
+  const auto &&real_v                = permute_vectors(even_indices, v1, v2);
+  const auto &&imag_v                = permute_vectors(odd_indices, v1, v2);
   return std::make_pair(real_v, imag_v);
 }
 
-__attribute__((always_inline, const)) inline constexpr static auto interleave_vectors(const __m512d v1, const __m512d v2) noexcept {
+__attribute__((always_inline)) inline constexpr static auto interleave_vectors(const auto v1, const auto v2) noexcept {
   // Create index vectors for interleaving
-  static constexpr const __m512i idx_real{0, 8, 1, 9, 2, 10, 3, 11};
-  static constexpr const __m512i idx_imag{4, 12, 5, 13, 6, 14, 7, 15};
+  static constexpr auto generate_indices = []<std::size_t S, std::size_t N, std::size_t add_even, std::size_t add_odd>() constexpr {
+    return []<std::size_t... I>(std::index_sequence<I...>) {
+      return std::index_sequence<(I % 2 == 0 ? S + I / 2 + add_even : S + I / 2 + add_odd)...>{};
+    }(std::make_index_sequence<N>{});
+  };
+  static constexpr auto N = sizeof(decltype(v1)) / sizeof(decltype(v1[0]));
+  //{0, 8, 1, 9, 2, 10, 3, 11}
+  static constexpr auto idx_real = generate_indices.template operator()<0 / 1, N, 0, N>();
+  //{4, 12, 5, 13, 6, 14, 7, 15}
+  static constexpr auto idx_imag = generate_indices.template operator()<N / 2, N, 0, N>();
   // Separate real and imaginary parts
-  const auto&& real_v = _mm512_permutex2var_pd(v1, idx_real, v2);
-  const auto&& imag_v = _mm512_permutex2var_pd(v1, idx_imag, v2);
+  const auto &&real_v = permute_vectors(idx_real, v1, v2);
+  const auto &&imag_v = permute_vectors(idx_imag, v1, v2);
   return std::make_pair(real_v, imag_v);
 }
 
-__attribute__((always_inline)) inline static auto load(std::complex<double> *const __restrict__ a) {
-  // Cast pointers to double*
-  const auto *const a0_ptr = reinterpret_cast<const double *>(a);
-  const auto *const a1_ptr = reinterpret_cast<const double *>(a + 4);
-
-  // Load arrays into AVX-512 registers
-  const auto&& va0 = _mm512_loadu_pd(a0_ptr);
-  const auto&& va1 = _mm512_loadu_pd(a1_ptr);
-
+// TODO: this can be more general, loading complex numbers of any size
+template <typename T, const size_t elems> __attribute__((always_inline)) inline static auto load(const std::complex<T> *const __restrict__ a) {
+  Vec<T, elems> va0{};
+  Vec<T, elems> va1{};
+  memcpy(&va0,  a, sizeof(T) * elems);
+  memcpy(&va1, a + elems / 2, sizeof(T) * elems);
   return std::make_pair(va0, va1);
 }
 
-__attribute__((always_inline)) inline constexpr static auto load_and_separate(std::complex<double> *__restrict__ a) {
-  const auto [va0, va1] = load(a);
+
+__attribute__((always_inline)) inline static void store(auto*  __restrict__ ptr, const auto v) {
+  static constexpr auto elems = sizeof(decltype(v)) / sizeof(decltype(v[0]));
+  using T = decltype(v[0]);
+  std::memcpy(ptr, &v, sizeof(T) * elems);
+}
+
+
+
+template<class T, size_t elems>
+__attribute__((always_inline)) inline constexpr static auto load_and_separate(const std::complex<T> *const __restrict__ a) {
+  const auto [va0, va1] = load<T, elems>(a);
   // Separate real and imaginary parts
-  const auto&& [real, imag] = separate_real_imaginary(va0, va1);
+  const auto [real, imag] = separate_real_imaginary(va0, va1);
   return std::make_pair(real, imag);
 }
 
-__attribute__((always_inline, const)) inline constexpr static auto set_vector_to_complex(const std::complex<double> a) {
-  return std::make_pair(_mm512_set1_pd(a.real()), _mm512_set1_pd(a.imag()));
+template <typename T> __attribute__((always_inline)) inline constexpr static auto set_vector_to_complex(const std::complex<double> a) {
+  return std::make_pair(T{a.real()}, T{a.imag()});
 }
 
-__attribute__((always_inline, const)) inline constexpr static auto complex_mul_avx512(const __m512d real0, const __m512d imag0, const __m512d real1,
-                                                                     const __m512d imag1) {
+__attribute__((always_inline)) inline constexpr static auto complex_mul(const auto real0, const auto imag0, const auto real1, const auto imag1) {
+  // check if the compiler is clang or gcc
   // (a+ib) * (c+id) = (ac - bd) + i(ad + bc)
   // (real0 + i*imag0) * (real1 + i*imag1) = (real0*real1 - imag0*imag1) + i*(real0*imag1 + imag0*real1)
   // Multiply complex numbers
-  const auto&& real = _mm512_fmsub_pd(real0, real1, _mm512_mul_pd(imag0, imag1));
-  const auto&& imag = _mm512_fmadd_pd(real0, imag1, _mm512_mul_pd(imag0, real1));
-  // Interleave real and imaginary parts
-  return std::make_pair(real, imag);
+  // Equivalent to normal multiplication but std library uses this algorithm.
+  // Benchmark: https://quick-bench.com/q/OV56ZJWeLE7nHQxRNFXO0-kGTtU
+#ifdef ENABLE_KARASUBA
+   auto ac = real0 * real1;
+   auto bd = imag0 * imag1;
+   auto &&real = ac - bd;
+   auto &&imag = __builtin_elementwise_fma(real0 + imag0, real1 + imag1, - ac - bd);
+#else
+   auto &&real = __builtin_elementwise_fma(real0, real1, -imag0 * imag1);
+   auto &&imag = __builtin_elementwise_fma(real0, imag1, imag0 * real1);
+#endif
+   return std::make_pair(real, imag);
 }
 
 #endif
