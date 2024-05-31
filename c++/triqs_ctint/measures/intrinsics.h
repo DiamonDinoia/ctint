ed5fa329e3558e9d75fbfaecb7ea2597c5fef046
DiamonDinoia <m.barbone19@imperial.ac.uk>
2024-05-30 23:14:39 -0400

    Added documentation

--- c++/triqs_ctint/measures/intrinsics.h
+++ c++/triqs_ctint/measures/intrinsics.h
@@ -9,6 +9,10 @@
 
 template <class T, std::uint8_t elems> using Vec = T __attribute__((vector_size(elems * sizeof(T))));
 
+/**
+ * @brief Get the widest simd type for the current architecture
+ * Currently only supports AVX512, AVX2 and SSE4.2 architectures
+ */
 template<class T>
 static constexpr uint widest_simd() noexcept {
   if constexpr (__AVX512F__) {
@@ -23,6 +27,11 @@ static constexpr uint widest_simd() noexcept {
   return 1U;
 }
 
+/**
+ *  This selects the indices I ... from v1 and v2
+ *  The indices are from 0 to 2N where N is the number of elements in the vector
+ *  if i < N then return v1[i] else return v2[i - N]
+ */
 template <std::size_t... I>
 __attribute__((always_inline)) inline static constexpr auto permute_vectors(std::index_sequence<I...>, const auto v1, const auto v2) noexcept {
   // I  -> [0, 2N] where N is the number of elements in the vector
@@ -31,7 +40,15 @@ __attribute__((always_inline)) inline static constexpr auto permute_vectors(std:
 
 }
 
+/**
+ * @brief Separate real and imaginary parts of a complex vector
+ * @param v1 vector containing real parts
+ * @param v2 vector containing imaginary parts
+ * @return std::pair<Vec<T, N>, Vec<T, N>> real and imaginary parts
+ */
 __attribute__((always_inline)) inline static constexpr auto separate_real_imaginary(const auto &v1, const auto &v2) noexcept {
+  // Create index vectors for separating real and imaginary parts using fold expressions
+  // it returns 0, 2, 4, 6, 8, 10, 12, 14 or 1, 3, 5, 7, 9, 11, 13, 15
   static constexpr auto generate_indices = []<std::size_t N, bool odd>() constexpr {
     return []<std::size_t... I>(std::index_sequence<I...>) {
       return std::index_sequence<(2 * I + static_cast<int>(odd))...>{};
@@ -40,13 +57,20 @@ __attribute__((always_inline)) inline static constexpr auto separate_real_imagin
   static constexpr auto N            = sizeof(decltype(v1)) / sizeof(decltype(v1[0]));
   static constexpr auto even_indices = generate_indices.template operator()<N, false>();
   static constexpr auto odd_indices  = generate_indices.template operator()<N, true>();
+  // Separate real and imaginary parts using the index vectors created above
   const auto &&real_v                = permute_vectors(even_indices, v1, v2);
   const auto &&imag_v                = permute_vectors(odd_indices, v1, v2);
   return std::make_pair(real_v, imag_v);
 }
 
 __attribute__((always_inline)) inline constexpr static auto interleave_vectors(const auto real, const auto imag) noexcept {
-  // Create index vectors for interleaving
+  // Create index vectors for interleaving real and imaginary parts using fold expressions
+  // it returns 0, N, 1, N+1, 2, N+2, 3, N+3 and so on
+  // S is the starting index
+  // N is the number of elements in the vector
+  // add_even is the offset for even indices
+  // add_odd is the offset for odd indices
+  // this is more general than what needed here as the offsets are always 0 and N respectively
   static constexpr auto generate_indices = []<std::size_t S, std::size_t N, std::size_t add_even, std::size_t add_odd>() constexpr {
     return []<std::size_t... I>(std::index_sequence<I...>) {
       return std::index_sequence<(I % 2 == 0 ? S + I / 2 + add_even : S + I / 2 + add_odd)...>{};
@@ -62,7 +86,10 @@ __attribute__((always_inline)) inline constexpr static auto interleave_vectors(c
   const auto &&res2 = permute_vectors(idx_imag, real, imag);
   return std::make_pair(res1, res2);
 }
-
+// Load two vectors of complex numbers
+// it uses memcpy to load the vectors to avoid reinterpret_cast
+// drawback is that it is not constexpr
+// compilers are really good at optimizing memcpy not worth using union puns
 // TODO: this can be more general, loading complex numbers of any size
 template <typename T, const size_t elems> __attribute__((always_inline)) inline static auto load(const std::complex<T> *const __restrict__ a) {
   Vec<T, elems> va0{};
@@ -72,13 +99,17 @@ template <typename T, const size_t elems> __attribute__((always_inline)) inline 
   return std::make_pair(va0, va1);
 }
 
-
+// Store two vectors of complex numbers
+// it uses memcpy to store the vectors to avoid reinterpret_cast
+// drawback is that it is not constexpr
+// compilers are really good at optimizing memcpy not worth using union puns
 __attribute__((always_inline)) inline static void store(auto*  __restrict__ ptr, const auto &v) {
   std::memcpy(ptr, &v, sizeof(decltype(v)));
 }
 
 
 
+// load two vectors of complex numbers and separate real and imaginary parts
 template<class T, size_t elems>
 __attribute__((always_inline)) inline constexpr static auto load_and_separate(const std::complex<T> *const __restrict__ a) {
   const auto [va0, va1] = load<T, elems>(a);
@@ -87,6 +118,8 @@ __attribute__((always_inline)) inline constexpr static auto load_and_separate(co
   return std::make_pair(real, imag);
 }
 
+// initialize two vectors of real numbers from a complex number
+// one vector for real part and one for imaginary part
 template <typename T> __attribute__((always_inline)) inline constexpr static auto set_vector_to_complex(const std::complex<double> a) {
   T real{};
   T imag{};
@@ -97,6 +130,8 @@ template <typename T> __attribute__((always_inline)) inline constexpr static aut
   return std::make_pair(real, imag);
 }
 
+// complex multiplication vectorized
+// this is the same code that std::complex uses for multiplication when -fcx-limited-range is enabled
 __attribute__((always_inline)) inline constexpr static auto complex_mul(const auto real0, const auto imag0, const auto real1, const auto imag1) {
   // check if the compiler is clang or gcc
   // (a+ib) * (c+id) = (ac - bd) + i(ad + bc)
@@ -108,6 +143,7 @@ __attribute__((always_inline)) inline constexpr static auto complex_mul(const au
    auto ac = real0 * real1;
    auto bd = imag0 * imag1;
    auto &&real = ac - bd;
+   // TODO: __builtin_elementwise_fma is clang specific
    auto &&imag = __builtin_elementwise_fma(real0 + imag0, real1 + imag1, - ac - bd);
 #else
    auto &&real = __builtin_elementwise_fma(real0, real1, -imag0 * imag1);
