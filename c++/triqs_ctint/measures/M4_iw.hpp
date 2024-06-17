c3b38ea62aca845911f427794dc24c6892993657
Marco Barbone <mbarbone@flatironinstitute.org>
2024-06-17 17:21:30 -0400

    using xsimd for now

--- c++/triqs_ctint/measures/M4_iw.hpp
+++ c++/triqs_ctint/measures/M4_iw.hpp
@@ -48,7 +48,7 @@ namespace triqs_ctint::measures {
     block_gf<prod<imfreq, imfreq>, matrix_valued, M_layout> M;
 
     private:
-    template <unsigned bl1_batch, unsigned bl2_batch> void accumulate(mc_weight_t sign, unsigned bl1, unsigned bl2);
+    void accumulate(mc_weight_t sign, unsigned bl1, unsigned bl2);
   };
 
 } // namespace triqs_ctint::measures
