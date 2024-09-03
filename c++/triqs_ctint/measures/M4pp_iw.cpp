9cec159c06d83b48b98d010f5dd0b14ebfa75933
Marco Barbone <mbarbone@flatironinstitute.org>
2024-09-03 16:41:16 -0400

    Fixed M4 plaquette

--- c++/triqs_ctint/measures/M4pp_iw.cpp
+++ c++/triqs_ctint/measures/M4pp_iw.cpp
@@ -51,7 +51,7 @@ namespace triqs_ctint::measures {
     for (int bl1 : range(params.n_blocks())) { // FIXME c++17 Loops
       for (int bl2 : range(params.n_blocks())) {
         auto const bl2_size = M[bl2].target_shape()[0];
-        simd::iw4_accumulate(sign, M, M4pp_iw_, bl1, bl2, bl2_size);
+        simd::iw4pp_accumulate(sign, M, M4pp_iw_, bl1, bl2, bl2_size);
       }
     }
   }
